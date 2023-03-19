badfe96f347ac59267bf23fff02e4366cd83e393
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-03-19 14:42:08 +0200

    corection

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ export class App extends Component {
       { id: 'id-1', userName: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', userName: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', userName: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', userName: 'Annie Copeland', number: '227-91-26' },
+      
     ],
     filter: '',
   };
